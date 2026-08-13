289c6ca1cef77f3e44ec6425ba569b47f553266b
LP <lp@thebonhomme.com>
2026-08-13 08:06:27 -0400

    Revert "checkpoint before checking out cursor/swarm-local-handoff-1c2f"
    This reverts commit acfbdbcda854e24f4fe8ab8a557caccadf85e0e7.

--- interventions/FlexAID_mifoff.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Wrapper: appends -c mif_off.json to the harness's fixed argv (no hook exists).
-exec /Users/lp.more/Projects/FlexAIDdS/ab_mac_20260806T133329/wt_post_cpu/build/FlexAID "$@" -c /Users/lp.more/Projects/FlexAIDdS/interventions/mif_off.json
